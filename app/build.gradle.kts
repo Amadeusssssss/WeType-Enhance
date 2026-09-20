bad8ae828727902bfa52f826104ada122e2a5f63
Amadeusssssss <45653884+Amadeusssssss@users.noreply.github.com>
2026-09-20 17:01:22 +0800

    chore(version): 规范版本号为 1.28.4-18key 并更新文档

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xposed.wetypehook"
         minSdk = 31
         targetSdk = 37
-        versionCode = 41
-        versionName = "1.29.0-18key"
+        versionCode = 12804
+        versionName = "1.28.4-18key"
     }
 
     buildTypes {
